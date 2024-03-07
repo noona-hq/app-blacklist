f4db03445ac8fdd9101c449c9a22372d912a3373
sverrirsig <svesig@gmail.com>
2024-03-07 23:40:40 +0000

    Direct connection false when running on environment

--- services/noona/config.go
+++ services/noona/config.go
@@ -2,7 +2,7 @@ package noona
 
 type Config struct {
 	BaseURL               string `default:"http://localhost:31140"`
-	AooStoreURL           string `default:"http://localhost:31130/week#settings-apps"`
+	AppStoreURL           string `default:"http://localhost:31130/week#settings-apps"`
 	ClientID              string `default:""`
 	ClientSecret          string `default:""`
 	BlacklistBaseURL      string `default:"http://localhost:8080"`
